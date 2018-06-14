ad588b9240593a5c568e22e7978c86ec38163c13
ckashby <clay.ashby@gmail.com>
2018-06-13 18:49:12 -0700

    Add css to increase margin for body - refactored nav into _header and _footer

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /tweets
   # GET /tweets.json
@@ -29,11 +30,9 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
-        format.json { render :show, status: :created, location: @tweet }
+        format.html { redirect_to tweets_path, notice: 'Tweet was successfully created.' }
       else
         format.html { render :new }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,11 +42,9 @@ class TweetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tweet }
+        format.html { redirect_to tweets_path, notice: 'Tweet was successfully updated.' }
       else
         format.html { render :edit }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -58,7 +55,6 @@ class TweetsController < ApplicationController
     @tweet.destroy
     respond_to do |format|
       format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
